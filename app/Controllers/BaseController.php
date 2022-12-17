a7c88275aabc7d6e77a55aa5c8d30a8fcb354492
Sander <info@webigniters.nl>
2022-12-17 23:41:55 +0100

    Added helper

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = ['form','text'];
+    protected $helpers = ['form','text', 'filesystem'];
 
     /**
      * Constructor.
